b9d7cb9e25afb4fba37d3835378916e3c84484de
edmondkipkorir <eddykarev@gmail.com>
2022-03-22 12:37:22 +0300

    added image on the staff section

---
+++ about.js
@@ -0,0 +1,19 @@
+$('#hide').hide();
+
+        $('#show1,#hide2').on('click',function(){
+
+        	$('#show1,#hide2').toggle()
+        });
+        $('#show1').show();
+
+        $('#show2,#hide1').on('click',function(){
+
+        	$('#show2,#hide1').toggle()
+        });
+        $('#hide2').hide();
+
+        $('#show2,#hide1').on('click',function(){
+
+        	$('#show2,#hide1').toggle()
+        });
+   
