a86a5d517d72e42ac0bd7f912666e6b4b08673b7
edmondkipkorir <eddykarev@gmail.com>
2022-03-23 13:47:41 +0300

    added javascript function

--- about.js
+++ about.js
@@ -1,19 +1,15 @@
-$('#hide').hide();
+$("#hide").hide();
 
-        $('#show1,#hide2').on('click',function(){
-
-        	$('#show1,#hide2').toggle()
-        });
-        $('#show1').show();
-
-        $('#show2,#hide1').on('click',function(){
-
-        	$('#show2,#hide1').toggle()
-        });
-        $('#hide2').hide();
-
-        $('#show2,#hide1').on('click',function(){
-
-        	$('#show2,#hide1').toggle()
-        });
+$("#show,#hide").on("click", function () {
+  $("#show,#hide").toggle();
+});
+$("#hide1").hide();
+        $(".overlay").hover(
+                function () {
+                  $(".overlay-text").show();
+                },
+                function () {
+                  $(".overlay-text").hide();
+                }
+              );
    
