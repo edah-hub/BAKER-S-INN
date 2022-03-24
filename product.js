0a1ef72dce943c5b793f3ac718d918e7d05bf603
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-24 10:51:34 +0300

    add some js ready function

--- product.js
+++ product.js
@@ -1,6 +1,17 @@
+//business logic
 function Cake(type,name,price,number){
     this.cakeType = type;
     this.cakeName = name;
     this.cakePrice = price;
     this.cakeNumber = number;
-}
+$(".detaills").hide();
+
+//user interface logic
+$(document).ready(function(){
+    $("h5").click(function(event){
+        event.preventDefault();
+    });
+
+
+
+});
