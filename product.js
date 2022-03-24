ad8378f8c35f8389c8c581fce168acab6008ade8
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-24 14:30:37 +0300

    add event listener to add to cart button

--- product.js
+++ product.js
@@ -9,8 +9,41 @@ $(".detaills").hide();
 
 //user interface logic
 $(document).ready(function(){
-    $("or").click(function(event){
+    $(".proceed1").click(function(event){
         event.preventDefault();
+        var cakeNamep = $("#1").text();
+        var index = cakeNamep.indexOf("Ksh");
+        var cakePrice = cakeNamep.slice(index + 4);
+        var cakeName = cakeNamep.slice(0,index);
+        alert("You ordered " + cakeName + " cake at Ksh " + cakePrice) ;
+
+    });
+    $(".proceed2").click(function(event){
+        event.preventDefault();
+        var cakeNamep = $("#2").text();
+        var index = cakeNamep.indexOf("Ksh");
+        var cakePrice = cakeNamep.slice(index + 4);
+        var cakeName = cakeNamep.slice(0,index);
+        alert("You ordered " + cakeName + "cake at Ksh " + cakePrice) ;
+
+    });
+    $(".proceed3").click(function(event){
+        event.preventDefault();
+        var cakeNamep = $("#3").text();
+        var index = cakeNamep.indexOf("Ksh");
+        var cakePrice = cakeNamep.slice(index + 4);
+        var cakeName = cakeNamep.slice(0,index);
+        alert("You ordered " + cakeName + " cake at Ksh " + cakePrice) ;
+
+    });
+    $(".proceed4").click(function(event){
+        event.preventDefault();
+        var cakeNamep = $("#4").text();
+        var index = cakeNamep.indexOf("Ksh");
+        var cakePrice = cakeNamep.slice(index + 4);
+        var cakeName = cakeNamep.slice(0,index);
+        alert("You ordered " + cakeName + " cake at Ksh " + cakePrice) ;
+
     });
 
 
